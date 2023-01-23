880de2a4239d4bb201faa68f576cc7a02c128bce
Guillermo García <guillermo.garcia@selloflex.com.mx>
2023-01-23 13:40:01 -0600

    1.0.2 build

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 import MongoDbInstance from './mongod.js';
 export { InitError, AuthError, NotFoundError } from './errors.js';
 export { MongoDbInstance } from './mongod.js';
-const DB = new MongoDbInstance('./.mongodb');
